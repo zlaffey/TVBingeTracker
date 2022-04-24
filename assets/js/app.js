f8786bda442bcc503958e5cd1c71572e92b1b852
zlaffey <zacklaffey@gmail.com>
2022-04-24 13:21:59 -0500

    updated styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,15 +11,15 @@ fetch(url)
 	.then((res) => res.json()) // parse response as JSON
 	.then((data) => {
 		console.log(data);
-		document.querySelector("h2").innerHTML = data.name;
-		document.querySelector("img").src = data.image.medium;
+		document.querySelector("h1").innerText = data.name;
+		document.querySelector("#showPoster").src = data.image.medium;
 		document.querySelector("#summarySection").innerHTML = data.summary;
 		data._embedded.cast.forEach((character) => {
 			const li = document.createElement("li");
 			const span = document.createElement("span");
 			span.textContent = character.person.name;
 			const castImg = document.createElement("img");
-			castImg.className = "headshot";
+			castImg.className = "image fit";
 			castImg.src = character.person.image.medium;
 			li.appendChild(castImg);
 			li.appendChild(span);
@@ -39,28 +39,38 @@ fetch(url2)
 	.then((data) => {
 		console.log(data);
 		data._embedded.episodes.forEach((episode) => {
-			const li = document.createElement("li");
-			li.className = "episodeItem";
+			const li = document.createElement("article");
+			li.className = "style2";
 			li.id = `ep#${episode.id - 40645}`;
 			li.addEventListener("click", watchShow);
 			const span = document.createElement("span");
-			span.className = "releaseDate";
-			span.textContent = episode.airdate;
-			const span2 = document.createElement("span");
-			span2.className = "epTitle";
-			span2.textContent = episode.name;
-			const epImg = document.createElement("img");
-			epImg.className = "epImg";
-			epImg.src = episode.image.medium;
+			span.className = "image";
+			span.innerHTML = `<img src=${episode.image.medium}>`;
+			const a = document.createElement("a");
+			const title = document.createElement("h2");
+			title.textContent = episode.name;
+			const content = document.createElement("div");
+			// content.className = "content";
+			// content.innerHTML = episode.summary;
+			// const span2 = document.createElement("span");
+			// span2.className = "epTitle";
+			// span2.textContent = episode.name;
+			// const epImg = document.createElement("img");
+			// epImg.className = "epImg";
+			// epImg.src = episode.image.medium;
 
+			a.appendChild(title);
+			// a.appendChild(content);
 			li.appendChild(span);
-			li.appendChild(span2);
-			li.appendChild(epImg);
+			li.appendChild(a);
+			// li.appendChild(span2);
+			// li.appendChild(epImg);
 
 			document.querySelector("#episodeList").appendChild(li);
 
 			function watchShow() {
-				li.classList.toggle("watched");
+				li.classList.toggle("style2");
+				li.classList.toggle("style3");
 				console.log(li.id);
 				watchedEpisodes.push(li.id);
 				console.log(watchedEpisodes);
